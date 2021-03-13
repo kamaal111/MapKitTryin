3570489544f33bac25d28d1c70f753989112f331
kamaal111 <kamaal.f1@gmail.com>
2021-03-13 13:33:55 +0100

    Initial commit

---
+++ MapKitTryin/Views/MainViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainViewController.swift
+//  MapKitTryin
+//
+//  Created by Kamaal M Farah on 13/03/2021.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .red
+    }
+
+}
