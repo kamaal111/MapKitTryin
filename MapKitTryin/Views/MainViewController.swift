e7b6f0e12925922dfccc5a07db44cf15b4d89701
kamaal111 <kamaal.f1@gmail.com>
2021-03-13 13:51:20 +0100

    Displaying map

--- MapKitTryin/Views/MainViewController.swift
+++ MapKitTryin/Views/MainViewController.swift
@@ -6,12 +6,33 @@
 //
 
 import UIKit
+import MapKit
 
 class MainViewController: UIViewController {
 
+    private lazy var mapView: MKMapView = {
+        let map = MKMapView()
+        map.translatesAutoresizingMaskIntoConstraints = false
+        return map
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .red
+        setupView()
+    }
+
+    private func setupView() {
+        self.view.addSubview(mapView)
+        setupConstraints()
+    }
+
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            mapView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            mapView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            mapView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            mapView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
     }
 
 }
